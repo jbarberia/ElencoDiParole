95d09b650f65a06b741404f0ef0e7c8b482943fd
jbarberia <jbarberia@frba.utn.edu.ar>
2021-04-09 17:32:46 -0300

    Sumar palabras en CSV

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, session
 from tempfile import mkdtemp
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWidgets import QApplication
 from threading import Timer
+import random
+import csv
 import sys
 
 # Configure app
@@ -17,11 +19,24 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 # Intialize DB (TODO: work with a CSV file)
-WORDS = [{'it': 'foo', 'es': 'bar', 'desc': ""}]
+WORDS = []
+with open("glossario.csv", "r", encoding='utf-8') as f:
+    header = [h.strip() for h in f.readline().split(';')]
+    reader = csv.reader(f, delimiter=";")
+    for row in reader:
+        entry = {}
+        values = [x.strip() for x in row]
+        entry[header[0]] = values[0]
+        entry[header[1]] = values[1]
+        entry[header[2]] = values[2]
+        
+        WORDS.append(entry)
+print(WORDS)
 
 # Write to main page
 @app.route("/")
 def index():
+    session['life'] = 5
     return render_template("index.html", words = WORDS)
 
 # Add Words
@@ -73,22 +88,56 @@ def remove():
         flash(f"Palabra Removida: {es}({it})")
         return redirect("/")
 
-#@app.route("/play", methods=["GET", "POST"])
-#def play():
-#    pass
 
+@app.route("/play", methods=["GET", "POST"])
+def play():
 
-## Define function for QtWebEngine
-#def ui(location):
-#    qt_app = QApplication(sys.argv)
-#    web = QWebEngineView()
-#    web.setWindowTitle("Elenco Di Parole")
-#    web.resize(600, 600)
-#    web.load(QUrl(location))
-#    web.show()
-#    sys.exit(qt_app.exec_())
-#    
-#if __name__ == "__main__":
-#    # start sub-thread to open the browser.
-#    Timer(1, lambda: ui("http://127.0.0.1:5000/")).start()
-#    app.run(debug = False)
+    if request.method == "GET":
+        word = random.choice(WORDS)
+        lang = {'es': 'español', 'it': 'italiano'}
+        
+        # Make selection
+        choice = random.randint(0, 1)
+        choice_lang = "es" if choice == 1 else "it"
+        guess_lang = "es" if choice == 0 else "it"
+
+        session['correct'] = word[guess_lang]
+        return render_template("game.html", lifes=session['life'], lang=lang[guess_lang], word=word[choice_lang])
+
+    if request.method == "POST":
+        ans = request.form.get("ans")
+        lifes = request.form.get("lifes")
+        correct = session['correct']
+
+        if not ans:
+            ans = ""
+
+        if not correct:
+            assert False
+
+        if ans.strip().lower() == correct.strip().lower():
+            flash(f"Respuesta Correcta! {correct} = {ans}")
+        else:
+            flash(f"Respuesta Incorrecta! La respuesta correcta era {session['correct']} y no {ans}.")
+            session['life'] = session['life'] - 1
+
+        if session['life'] < 1:
+            flash("Perdiste!")
+            return redirect("/")
+        
+        return redirect("/play")
+        
+# Define function for QtWebEngine
+def ui(location):
+    qt_app = QApplication(sys.argv)
+    web = QWebEngineView()
+    web.setWindowTitle("Elenco Di Parole")
+    web.resize(600, 600)
+    web.load(QUrl(location))
+    web.show()
+    sys.exit(qt_app.exec_())
+    
+if __name__ == "__main__":
+    # start sub-thread to open the browser.
+    Timer(1, lambda: ui("http://127.0.0.1:5000/")).start()
+    app.run(debug = False)
